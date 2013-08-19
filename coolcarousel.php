19ca12a531813bbe37700bb29b685d8a3654709c
cliffordp <tko@tourkick.com>
2013-08-19 11:02:24 -0500

    back to adding a plugin wrapper
    added a plugin wrapper in a previous commit, then undid that, now
    re-doing that.

---
+++ coolcarousel.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Cool Carousel PL Section
+Author: TourKick
+Author URI: http://tourkick.com/?utm_source=pagelines&utm_medium=section&utm_content=authoruri&utm_campaign=coolcarousel_section
+Plugin URI: http://www.pagelinestheme.com/coolcarousel-section?utm_source=pagelines&utm_medium=section&utm_content=pluginuri&utm_campaign=coolcarousel_section
+Description: A custom post type section that allows images, videos, or custom HTML in a horizontal, fade, or vertical carousel (i.e. slider). Responsive, multiple displayed at once, customizable number of slides to advance, auto play option, timing intervals, and many more carousel-by-carousel options.
+Demo: http://www.pagelinestheme.com/coolcarousel-section?utm_source=pagelines&utm_medium=section&utm_content=demolink&utm_campaign=coolcarousel_section
+Class Name: CoolCarousel
+Version: 1.0.20130729
+*/
